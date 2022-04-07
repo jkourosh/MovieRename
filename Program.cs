6760167fc54f85c5bdbcb1b1c0b9dc871bf32b03
jkourosh <jkourosh@gmail.com>
2022-04-07 20:40:51 +0430

    init

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("********************************************");
+Console.WriteLine("    This help to Category Movies on Year");
+Console.WriteLine("          Last Version 2022");
+Console.WriteLine("********************************************");
+Console.WriteLine("        Press a key to Continue ...");
+Console.ReadKey();
+
+string pattern = @"(19|20)\d{2}";
+string moviePattern = @"/^(?!\d\d?[ex]\d\d?)(?:\[(?:[-\w\s]+)*\])?(.*?)[-_.]?(?:[\{\(\[]?(?:dvdrip|[-._\b]ita|[-._\b]eng|xvid|cd\d|dvdscr|\w{1,5}rip|divx|\d+p|\d{4}).*?)?\.([\w]{2,3})$/img";
+
+DirectoryInfo d = new(Directory.GetCurrentDirectory());
+FileInfo[] infos = d.GetFiles("*.*");
+
+foreach (FileInfo f in infos)
+{
+    var mov = Regex.Match(f.Name, moviePattern);
+    if (mov.Success)
+    {
+        var m = Regex.Match(f.Name, pattern);
+        if (m.Success)
+        {  
+            var r = Regex.Replace(f.Name, pattern, "");
+            string newName = Path.Combine(f.DirectoryName.ToString(), m.ToString() + "." + r.ToString());
+            newName = newName.Replace("()", "");
+            newName = newName.Replace("[]", "");
+            newName = newName.Replace("-.", ".");
+            newName = newName.Replace(".-", ".");
+            newName = newName.Replace("..", ".");
+            File.Move(f.FullName, newName);
+            Console.WriteLine(newName);
+        }
+
+    }
+}
+// Console.WriteLine("There isn't any Movie in this Dir!");
+
+Console.ReadKey();
+
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# MovieRename
-Raneme Movies In Current Folder Batch
+# Movie Rename
+Raneme Movies Based on Date ,In Current Folder Batch
+
+
+[![GitHub license](https://img.shields.io/github/license/microsoft/Web-Dev-For-Beginners.svg)](https://github.com/microsoft/Web-Dev-For-Beginners/blob/master/LICENSE)
+
+
+
+
+#### Rename Movie in Folder based on Date - C#.
+
+
+## *Author Name*
+[Kourosh](https://github.com/jkourosh)
+
+
+## Deploy your's
+
+You can deploy, or publish your program .
+
+1. Fork this repo
+2. Review  through the Code then creating your Idea.
+3. Open up files with your favorite text editor and hack away. It's that simple.
+
